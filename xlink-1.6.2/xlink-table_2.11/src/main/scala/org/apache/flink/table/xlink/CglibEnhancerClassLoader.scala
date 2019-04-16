dcc8ec3c9795303e3d01dcef87d297db9c0e7bd5
dongjiaqiang <martin.dong@ximalaya.com>
2019-04-16 22:18:44 +0800

    fix

--- xlink-1.6.2/xlink-table_2.11/src/main/scala/org/apache/flink/table/xlink/CglibEnhancerClassLoader.scala
+++ xlink-1.6.2/xlink-table_2.11/src/main/scala/org/apache/flink/table/xlink/CglibEnhancerClassLoader.scala
@@ -1,9 +1,9 @@
 package org.apache.flink.table.xlink
 
-import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.{JSON, JSONObject}
 import net.sf.cglib.core.ReflectUtils
 import org.apache.commons.codec.binary.Base64
-
+import scala.collection.convert.wrapAsScala._
 /**
   *
   * @author martin.dong
@@ -11,28 +11,51 @@ import org.apache.commons.codec.binary.Base64
   **/
 class CglibEnhancerClassLoader(val parent:ClassLoader, val dynamicUDFInfo:String) extends ClassLoader(parent){
   require(dynamicUDFInfo != "")
-  private val udfInfoMap: Map[String, String] = {
-    val jsonObject = JSON.parseObject(dynamicUDFInfo)
-    import scala.collection.convert.wrapAsScala._
-    jsonObject.keySet().map(key ⇒ {
-      val value = jsonObject.
-        getString(key)
-      (key, value)
-    }).toMap
+
+  private def resolveUDFInfo(dynamicUDFInfo:String):Map[String,JSONObject]={
+      val resolveObject = JSON.parseObject(dynamicUDFInfo)
+      resolveObject.keySet().map(key⇒{
+        (key,resolveObject.getJSONObject(key))
+      }).toMap
+  }
+
+  private val dependencyUdfInfoMap:Map[String,String]= {
+    resolveUDFInfo(dynamicUDFInfo).toList.flatMap { case (_, data) ⇒
+      val dependencies = data.getJSONObject("dependency")
+      dependencies.keys.map(dependency ⇒ {
+        val dependencyData = dependencies.getString(dependency)
+        (dependency, dependencyData)
+      })
+    }.toMap
+  }
+
+  private val udfInfoMap: Map[String,(String,Set[String])] = {
+    resolveUDFInfo(dynamicUDFInfo).toList.map { case (name, jsonObject) ⇒
+      val bytes = jsonObject.getString("data")
+      val dependencies = jsonObject.getJSONObject("dependency")
+      (name, (bytes, dependencies.keySet().toSet))
+    }.toMap
   }
 
   override def findClass(className: String): Class[_] = {
-    val name = className.replace("/",".")
-    if(name.contains("EnhancerByCGLIB")) {
-      udfInfoMap.keySet.filter(_ != name).foreach(c ⇒ this.loadClass(c))
-    }
-    udfInfoMap.get(name).map(value ⇒ {
-      val byteValue = Base64.decodeBase64(value)
+    val name = className.replace("/", ".")
+    if (name.contains("EnhancerByCGLIB")) {
+      udfInfoMap(name)._2.foreach(c ⇒ this.loadClass(c))
+      val byteValue = Base64.decodeBase64(udfInfoMap(name)._1)
       try {
         ReflectUtils.defineClass(name, byteValue, parent)
-      }catch {
-        case _:Exception⇒parent.loadClass(name)
+      } catch {
+        case _: Exception ⇒ parent.loadClass(name)
       }
-    }).orNull
+    } else if(dependencyUdfInfoMap.contains(name)){
+      val byteValue = Base64.decodeBase64(dependencyUdfInfoMap(name))
+      try {
+        ReflectUtils.defineClass(name, byteValue, parent)
+      } catch {
+        case _: Exception ⇒ parent.loadClass(name)
+      }
+    }else{
+      null
+    }
   }
 }
